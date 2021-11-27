c969e641c81c687a5c8eb979c85d1e89f8a777f3
Dima Tytenko <amgpetronass@gmail.com>
2021-11-27 18:40:17 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Section } from './components/Section';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Section />
       </header>
     </div>
   );
---
+++ src/components/Section/Section.js
@@ -0,0 +1,4 @@
+function Section() {
+  return <h2>hello</h2>;
+}
+export default Section;
