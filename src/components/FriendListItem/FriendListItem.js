27995265aaeb0ca6d1210219b312021901cb5a9d
Dima Tytenko <amgpetronass@gmail.com>
2021-12-06 19:55:55 +0200

    fixed

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,13 +1,13 @@
 import s from './FriendListItem.module.css';
-import classnames from 'classnames';
+import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
+
+let cx = classNames.bind(s);
 
 export default function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className={s.item}>
-      <span
-        className={classnames(s.status, { [s['status-onlaine']]: isOnline })}
-      ></span>
+      <span className={cx('status', { 'status-onlaine': isOnline })}></span>
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
     </li>
